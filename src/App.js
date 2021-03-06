862a38439f04e5822ac091f21c355eb0bc597228
sumit Kumar <55837044+sumitkumar9988@users.noreply.github.com>
2021-03-06 22:41:29 +0530

    add more views login signup 404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import LandingPage from "./LandingPage/Index";
 import PortfolioPage from "./PortfolioPage/Index";
 import Dashboard from './Dashboard/Index'
+import Login from './authPage/Login'
+import FourOFour from './extraPage/FourOFour'
 import "./styles/index.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
@@ -10,8 +12,12 @@ function App() {
       <Router>
         <Switch>
           <Route path="/user" component={PortfolioPage} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route path="/basic" component={form}/>
           <Route path="/dashboard" component={Dashboard}/>
-          <Route path="/" component={LandingPage} />
+          <Route exact path="/" component={LandingPage} />
+          <Route  component={FourOFour}/>
         </Switch>
       </Router>
     </div>
--- src/Dashboard/Index.js
+++ src/Dashboard/Index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import HomePage from './views/HomePage'
+import Card from './components/Card'
 const Index = () => {
     return (
         <div>
             <HomePage/>
+            <Card/>
         </div>
     )
 }
