cf0900abfdc27726c21d5d90db273851cb47ae42
sumit Kumar <55837044+sumitkumar9988@users.noreply.github.com>
2021-04-15 06:58:51 +0530

    dom for wildcard subdomin

--- src/LandingPage/Components/components/RightImageItem.js
+++ src/LandingPage/Components/components/RightImageItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RightImageItem = ({ image, title, description }) => {
   return (
     <div>
-        <div class="flex flex-col w-full bg-black md:flex-row">
+       {title?<div class="flex flex-col w-full bg-black md:flex-row">
         <div class="flex flex-col items-center justify-center w-full p-5 md:w-1/2 lg:p-16 xl:p-24">
           <h2 class="max-w-lg text-4xl font-bold leading-tight text-blue-500 md:text-3xl lg:text-4xl">{title}</h2>
           <p class="mt-4 mb-10 text-white">{description}</p>
@@ -14,7 +14,7 @@ const RightImageItem = ({ image, title, description }) => {
             class="hover:overflow-x-hidden  duration-500 ease-in-out transform hover:scale-110 inset-0 object-cover "
           />
         </div>
-      </div>
+      </div>:null} 
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,14 +3,29 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import PortfolioPage from './PortfolioPage/Index';
 import { Provider } from 'react-redux';
 import store from './store';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-);
+const parsedData = window.location.host.split('.');
+
+if (parsedData.length >= 3) {
+  const subDomain = parsedData[0];
+  console.log('subdomin', subDomain);
+  ReactDOM.render(<PortfolioPage subDomain={subDomain} />, document.getElementById('root'));
+} else {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root'),
+  );
+}
 
 reportWebVitals();
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root'),
