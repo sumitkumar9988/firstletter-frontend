81691770d88d950ef5ba7b321c5579975c50f62a
sumit <55837044+sumitkumar9988@users.noreply.github.com>
2021-06-06 18:33:34 +0530

    gtag

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import LandingPage from './LandingPage/Index';
 import Dashboard from './Dashboard/Index';
 import Login from './authPage/Login';
--- src/LandingPage/Index.js
+++ src/LandingPage/Index.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from './View/Hero';
 import Features from './View/Features';
 import Footer from './Components/Footer';
 import Newletter from './Components/Newletter.js';
 import Client from './Components/Client';
+
 export default () => {
   return (
     <div className="bg-black">
